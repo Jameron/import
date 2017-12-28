ec5d2d3ac2697daec8dc7baa53a4113f82ba1040
cameron macfarlane <cammac1984@gmail.com>
2017-12-28 14:43:34 -0800

    fixed missing comma

--- src/config/import.php
+++ src/config/import.php
@@ -1,16 +1,16 @@
 <?php
 
 $imports = [
-'db_table_prefix' => '',
-'post_upload_route' => '/import'
-    'roles' => [
-        'admin' => [
-            'enabled' => true
-        ],
-        'user' => [
-            'enabled' => false
-        ],
-    ],
+	'db_table_prefix' => '',
+	'post_upload_route' => '/import',
+	'roles' => [
+		'admin' => [
+			'enabled' => true
+		],
+		'user' => [
+			'enabled' => false
+		],
+	],
 ];
 
 return $imports;
