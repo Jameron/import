f9eeaf44e2cdae2bc635ef43d9a9e4a890e2b872
cameron macfarlane <cammac1984@gmail.com>
2017-12-28 16:36:53 -0800

    testing updates, debugging

--- src/resources/views/partials/forms/import.blade.php
+++ src/resources/views/partials/forms/import.blade.php
@@ -2,10 +2,8 @@
 	<input data-toggle="custom-file" data-target="#csv" type="file" name="csv" accept=".csv" class="custom-file-input">
 	<span id="csv" class="custom-file-control custom-file-name" data-content="Upload csv file..."></span>
 </label>
-<div class="form-group">
-    <div class="col-md-6 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
-            Import
-        </button>
-    </div>
-</div>
+<p class="form-group">
+	<button type="submit" class="btn btn-primary">
+		Import
+	</button>
+</p>
