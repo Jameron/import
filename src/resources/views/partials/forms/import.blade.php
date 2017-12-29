21dfb9da77b0d48926a53a66745b5dc5100ba3be
cameron macfarlane <cammac1984@gmail.com>
2017-12-28 16:31:39 -0800

    added import button

--- src/resources/views/partials/forms/import.blade.php
+++ src/resources/views/partials/forms/import.blade.php
@@ -2,3 +2,10 @@
 	<input data-toggle="custom-file" data-target="#csv" type="file" name="csv" accept=".csv" class="custom-file-input">
 	<span id="csv" class="custom-file-control custom-file-name" data-content="Upload csv file..."></span>
 </label>
+<div class="form-group">
+    <div class="col-md-6 col-md-offset-4">
+        <button type="submit" class="btn btn-primary">
+            Import
+        </button>
+    </div>
+</div>
