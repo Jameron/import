3004c0d68026ecd58a26210a551f55aa34d0eef6
cameron macfarlane <cammac1984@gmail.com>
2017-12-28 16:23:57 -0800

    fixed the html

--- src/resources/views/partials/forms/import.blade.php
+++ src/resources/views/partials/forms/import.blade.php
@@ -1,4 +1,4 @@
 <label class="custom-file d-block">
-	<input data-toggle="custom-file" data-target="#company-logo" type="file" name="company_logo" accept="image/png" class="custom-file-input">
-	<span id="company-logo" class="custom-file-control custom-file-name" data-content="Upload company logo..."></span>
+	<input data-toggle="custom-file" data-target="#csv" type="file" name="csv" accept=".csv" class="custom-file-input">
+	<span id="csv" class="custom-file-control custom-file-name" data-content="Upload csv file..."></span>
 </label>
