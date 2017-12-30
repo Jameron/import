833689a060ae8b395622881fc18ae9f77f2c0ecf
cameron macfarlane <cammac1984@gmail.com>
2017-12-29 17:22:41 -0800

    testing updates, debugging

--- src/Import/Http/Controllers/ImportController.php
+++ src/Import/Http/Controllers/ImportController.php
@@ -14,28 +14,63 @@ use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
 class ImportController extends Controller
 {
+
+    protected $csv_data;
+
 	public function getImport()
 	{
 		return view('import::upload');
 	}
 
-	public function postImport()
+    public function cleanCsvHeadersData($raw_column_headers)
+    {
+        $trimmed_column_headers = array_map('trim', $raw_column_headers);
+        $pound_symbol_to_word_number_column_headers = str_replace("#", "number", $trimmed_column_headers);
+        $spaces_to_underscores_column_headers = str_replace(' ', '_', $pound_symbol_to_word_number_column_headers);
+        $letters_only_column_headers = preg_replace('/[^a-zA-Z_]/', '', $spaces_to_underscores_column_headers);
+        $lowercased_column_headers = array_map('strtolower', $letters_only_column_headers);
+
+        return $lowercased_column_headers;
+    }
+
+	public function postImport(Request $request)
 	{ 
 
 		$model = resolve('App\ImportModel');
-		$modelColumnsArray = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
-		dd($modelColumnsArray);
+		$model_columns_array = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
 
+        $csv = $request->file('csv');
+        $csv_array = array_map('str_getcsv', file($csv));
 
-		array_shift($modelColumnsArray);
+        $num_rows = count($csv_array);
+        $raw_column_headers = array_shift($csv_array);
+        $column_headers = $this->cleanCsvHeadersData($raw_column_headers);
+        $first_row = $csv_array[0];
+        
+        $relationships = config('import.relationships');
 
-			/*
-             *if (array_key_exists('name', $values)) {
-             *    $values = array_intersect_key($values, array_flip($modelColumnsArray));
-             *} else {
-             *    $values = array_combine(array_intersect_key($modelColumnsArray, $values), array_intersect_key($values, $modelColumnsArray));
-             *}
-			 */
+        foreach($column_headers as $key => $column_header) {
+
+            if(in_array($column_header, $model_columns_array)) {
+                $model->{$column_header} = $first_row[$key];
+            }
+
+            $related_key = array_search($column_header, array_column($relationships, 'csv_column'));
+
+            if(is_numeric($related_key)) {
+
+                $found_matching_related = DB::table($relationships[$related_key]['reference_table'])
+                    ->where($relationships[$related_key]['reference_field'], $first_row[$key])
+                    ->first();
+
+                if($found_matching_related) {
+                    $model->{$relationships[$related_key]['foreign_key']} = $found_matching_related->{$relationships[$related_key]['reference_primary_key']}; 
+                } 
+
+            }
+        }
+
+        dd($model);
 
 		return view('import::upload');
 	}
