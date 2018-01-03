efbf6c709d4595edc777f8600e6074c5d7ebf356
cameron macfarlane <cammac1984@gmail.com>
2018-01-02 17:31:29 -0800

    added in updates to make it more customizable

--- src/Import/Http/Controllers/ImportController.php
+++ src/Import/Http/Controllers/ImportController.php
@@ -4,73 +4,168 @@ namespace Jameron\Import\Http\Controllers;
 
 use DB;
 use Auth;
+use Validator;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Jameron\Regulator\Models\Permission;
 use App\Http\Controllers\Controller;
-use Jameron\Regulator\Http\Requests\PermissionRequest;
-use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
 class ImportController extends Controller
 {
 
     protected $csv_data;
+    protected $column_headers;
 
 	public function getImport()
 	{
 		return view('import::upload');
 	}
 
-    public function cleanCsvHeadersData($raw_column_headers)
+    public function setColumnHeaders($headers)
     {
-        $trimmed_column_headers = array_map('trim', $raw_column_headers);
-        $pound_symbol_to_word_number_column_headers = str_replace("#", "number", $trimmed_column_headers);
-        $spaces_to_underscores_column_headers = str_replace(' ', '_', $pound_symbol_to_word_number_column_headers);
-        $letters_only_column_headers = preg_replace('/[^a-zA-Z_]/', '', $spaces_to_underscores_column_headers);
-        $lowercased_column_headers = array_map('strtolower', $letters_only_column_headers);
-
-        return $lowercased_column_headers;
+        $this->column_headers = $headers;
+        return $this;
     }
 
-	public function postImport(Request $request)
-	{ 
+    public function cleanCsvHeadersData($raw_column_headers)
+    {
 
-		$model = resolve('App\ImportModel');
-		$model_columns_array = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
+        $cleanup_rules = config('import.csv_headers_cleanup_rules');
 
-        $csv = $request->file('csv');
-        $csv_array = array_map('str_getcsv', file($csv));
-
-        $num_rows = count($csv_array);
-        $raw_column_headers = array_shift($csv_array);
-        $column_headers = $this->cleanCsvHeadersData($raw_column_headers);
-        $first_row = $csv_array[0];
-        
-        $relationships = config('import.relationships');
-
-        foreach($column_headers as $key => $column_header) {
-
-            if(in_array($column_header, $model_columns_array)) {
-                $model->{$column_header} = $first_row[$key];
+        if(isset($cleanup_rules) && is_array($cleanup_rules)) {
+            if(in_array('trim', $cleanup_rules)) {
+                $this->setColumnHeaders(array_map('trim', $this->column_headers));
             }
 
-            $related_key = array_search($column_header, array_column($relationships, 'csv_column'));
+            if(in_array('pound_to_word_number', $cleanup_rules)) {
+                $this->setColumnHeaders(str_replace("#", "number", $this->column_headers));
+            }
 
-            if(is_numeric($related_key)) {
+            if(in_array('spaces_to_underscores', $cleanup_rules)) {
+                $this->setColumnHeaders(str_replace(' ', '_', $this->column_headers));
+            }
 
-                $found_matching_related = DB::table($relationships[$related_key]['reference_table'])
-                    ->where($relationships[$related_key]['reference_field'], $first_row[$key])
-                    ->first();
+            if(in_array('remove_special_characters', $cleanup_rules)) {
+                $this->setColumnHeaders(preg_replace('/[^a-zA-Z_]/', '', $this->column_headers));
+            }
 
-                if($found_matching_related) {
-                    $model->{$relationships[$related_key]['foreign_key']} = $found_matching_related->{$relationships[$related_key]['reference_primary_key']}; 
-                } 
-
+            if(in_array('lowercase', $cleanup_rules)) {
+                $this->setColumnHeaders(array_map('strtolower', $this->column_headers));
             }
         }
+    }
 
-        dd($model);
+    public function postImport(Request $request)
+    { 
+
+        $model = resolve('App\ImportModel');
+        $model_columns_array = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
+
+        $csv = $request->file('csv');
+        $csv_rows = array_map('str_getcsv', file($csv));
+
+        $num_rows = count($csv_rows);
+        $raw_column_headers = array_shift($csv_rows);
+        $this->setColumnHeaders($raw_column_headers);
+        $this->cleanCsvHeadersData($this->column_headers);
+
+        foreach($csv_rows as $row_index => $row) {
+
+            $relationships = config('import.relationships');
+            foreach($this->column_headers as $key => $column_header) {
+
+                if(in_array($column_header, $model_columns_array)) {
+                    $model->{$column_header} = $row[$key];
+
+                }
+
+                $related_key = array_search($column_header, array_column($relationships, 'csv_column'));
+                if(is_numeric($related_key)) {
+
+                    $matching_related = DB::table($relationships[$related_key]['reference_table'])
+                        ->where($relationships[$related_key]['reference_field'], $row[$key])
+                        ->first();
+
+                    if(!$matching_related && $relationships[$related_key]['create_if_not_found']) {
+
+                        $new_model = new $relationships[$related_key]['model'];
+                        $related_model_data_as_array = [];
+
+                        if (isset($relationships[$related_key]['extra_columns']) && is_array($relationships[$related_key]['extra_columns'])) {
+
+                            foreach($relationships[$related_key]['extra_columns'] as $extra_column) {
+
+                                $column_matching_key = array_search($extra_column['column'], $this->column_headers);
+                                $matching_data = $row[$column_matching_key];
+
+                                if($column_matching_key && $matching_data) {
+
+                                    if(is_array($extra_column['maps_to']) && isset($extra_column['explode_on'])) {
+
+                                        if (strpos(trim($matching_data), $extra_column['explode_on']) !== false) {
+                                            $split_as_array = explode($extra_column['explode_on'], $matching_data);
+                                            if(count($split_as_array)==2){
+                                                $result = array_combine($extra_column['maps_to'], $split_as_array);
+                                            }
+                                        }
+
+                                    } else if(is_string($extra_column['maps_to'])) {
+
+                                        $result = array($extra_column['maps_to'] => $matching_data);
+
+                                    }
+                                }
+
+                                if(isset($result)) {
+
+                                    // validate that the column matchup exists on the related table
+                                    $modelColumnsArray = $new_model->getConnection()->getSchemaBuilder()->getColumnListing($new_model->getTable());
+                                    $values = array_intersect_key($result, array_flip($modelColumnsArray));
+
+                                    if($values) {
+                                        foreach($values as $value_key => $value) {
+                                            $new_model->{$value_key} = $value;
+                                            $related_model_data_as_array[$value_key] = $value;
+                                        }
+                                    }
+
+                                }
+                            }
+
+                            $messages = [
+                                'email' => 'The :attribute address: ' . $related_model_data_as_array['email'] . ' on row ' . ($row_index + 2) . ' is invalid.',
+                            ];
+
+                            if (isset($relationships[$related_key]['validator']) && !empty($relationships[$related_key]['validator'])) {
+                                $rules = ((new $relationships[$related_key]['validator'])->rules());
+
+                                if($rules) {
+                                    $validator = Validator::make($related_model_data_as_array, $rules, $messages);
+
+                                    if ($validator->fails()) {
+                                        return redirect('import')->withErrors($validator->messages());
+                                    }
+                                }
+                            }
+
+                            if (isset($relationships[$related_key]['append_data']) && is_array($relationships[$related_key]['append_data'])) {
+                                foreach($relationships[$related_key]['append_data'] as $column_name => $column_data) {
+                                    $new_model->{$column_name} = $column_data;
+                                }
+                            }
+                            $new_model->save();
+
+                            $matching_related = $new_model;
+                        }
+
+                    }
+
+                    if($matching_related) {
+                        $model->{$relationships[$related_key]['foreign_key']} = $matching_related->{$relationships[$related_key]['reference_primary_key']}; 
+                    } 
+                }
+            }
+
+        }
 
 		return view('import::upload');
 	}
--- src/config/import.php
+++ src/config/import.php
@@ -1,21 +1,20 @@
 <?php
 // Sample CSV
-// Student ID, Test Name, Score, Date Taken
-// 123456, Beginning MySQL, 80, 11-12-2017
+// Student ID, Test Name, Score, Student Name, Date Taken
+// 123456, Beginning MySQL, 80, Bart Simpson, 11-12-2017
 
 $imports = [
     'get_import_route' => '/import',
     'post_import_route' => '/import',
-    'roles' => [
-        'admin' => [
-            'enabled' => true
-        ],
-        'user' => [
-            'enabled' => false
-        ],
+    'roles_enabled_for' => [
+        'admin' 
     ],
     /*
-     * The import model will be used to match your csv column headers to the table column names. Note, column headers are trimmed of leading and trailing white space, lowercased, spaces converted to underscores, and stripped of any special character.
+     *  The import model will be used to match your csv column headers to the table column names. 
+     *  Note, column headers are trimmed of leading and trailing white space, spaces converted to underscores, stripped of any special character, and lowercased. 
+    */
+    'csv_headers_cleanup_rules' => ['trim','pound_to_word_number','spaces_to_underscores', 'remove_special_characters','lowercase'],
+    /*
      * This mock model QuizScores might point to a table called quiz_scores
      * This quiz_scores table might have the following columns: [id, score, quiz_id, student_id, date_taken]
      * quiz_id and student_id are foreign keys that point to a quizs table and a users table respectively.
@@ -28,20 +27,39 @@ $imports = [
      */
     'relationships' => [
         [
-            'relationship' => 'belongsTo'
             'csv_column' => 'student_id',
             'reference_table' => 'users',
             'reference_field' => 'student_identification_number',
             'reference_primary_key' => 'id',
-            'foreign_key' => 'student_id'
-        ] 
+            'foreign_key' => 'student_id',
+            'model' => App\Models\User::class,
+            'validator' => App\Http\Requests\UserRequest::class,
+            'create_if_not_found' => true,
+            'append_data' => [
+                [
+                    'password' => Hash::make('ChangeIt!')
+                ]
+            ],
+            'extra_columns' => [
+                [
+                    'column' => 'student_name',
+                    'maps_to' => ['first_name','last_name'],
+                    'explode_on' => ' '
+                ],
+                [
+                    'column' => 'email',
+                    'maps_to' => 'email',
+                ]
+            ]
+        ],
         [
-            'relationship' => 'belongsTo'
             'csv_column' => 'test_name',
             'reference_table' => 'tests',
             'reference_field' => 'name', // This assumes that the name field on the tests table has a rule that forces unique
             'reference_primary_key' => 'id', 
-            'foreign_key' => 'test_id'
+            'foreign_key' => 'test_id',
+            'model' => App\Models\Tests::class,
+            'validator' => App\Http\Requests\TestRequest::class,
         ] 
     ]
 
