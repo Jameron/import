e8958efab2b32cf2c5ff8dc8698fd3589d7ef8a2
cameron macfarlane <cammac1984@gmail.com>
2018-01-04 17:35:45 -0800

    added cleanup

--- src/Import/Http/Controllers/ImportController.php
+++ src/Import/Http/Controllers/ImportController.php
@@ -36,28 +36,31 @@ class ImportController extends Controller
 
         foreach($csv_rows as $row_index => $row) {
 
-            $model = $this->parseRow($row, $row_index);
+            $response = $this->parseRow($row, $row_index);
 
-            $model_rules = (new $import_config['validator'])
-                ->rules();
+            if($response instanceof $import_config['import_model']) {
 
-            if($model_rules) {
+                $model_rules = (new $import_config['validator'])
+                    ->rules();
 
-                $validator = Validator::make($model->toArray(), $model_rules);
+                if($model_rules) {
 
-                if ($validator->fails()) {
-                    $errors->push($validator->messages());
-                } else {
-                    $model->save();
+                    $validator = Validator::make($response->toArray(), $model_rules);
+
+                    if ($validator->fails()) {
+                        $errors->push($validator->messages());
+                    } else {
+                        $response->save();
+                    }
                 }
-            }
 
-            if(count($errors)) {
-                return redirect('import')->withErrors($errors);
+            } else {
+                 $errors->push($validator->messages());
             }
         }
 
-		return view('import::upload');
+        return redirect('import')->withErrors($errors);
+
 	}
 
     public function parseRow($row, $row_index)
@@ -71,24 +74,38 @@ class ImportController extends Controller
 
         foreach($this->column_headers as $key => $column_header) {
 
-
             if(in_array($column_header, $model_columns_array)) {
+
+                $column_type = DB::getSchemaBuilder()->getColumnType($model->getTable(), $column_header);
+
+                if($column_type=="boolean") {
+                    $row[$key] = (trim(strtolower($row[$key]))=='yes') ? 1 : 0; 
+                } else if ($column_type=="datetime") {
+                    if( strtotime($row[$key])) {
+                        $row[$key] = date( 'Y-m-d H:i:s', strtotime($row[$key])); 
+                    } else {
+                        $row[$key] = null;
+                    }
+                } else if ($column_type=="integer") {
+                    $row[$key] = (trim($row[$key])=='') ? 0 : null; 
+                }
                 $model->{$column_header} = $row[$key];
             }
 
             if(count($relationships)) {
 
                 $related_key = array_search($column_header, array_column($relationships, 'csv_column'));
-                $related_model = $this->parseRelationships($related_key, $relationships, $row, $key);
 
-                if($related_model) {
-                    $model->{$relationships[$related_key]['foreign_key']} = $related_model->{$relationships[$related_key]['reference_primary_key']};
-                } else {
-                    continue;
-                }
+                if(is_numeric($related_key)) {
 
+                    $related_model = $this->parseRelationships($related_key, $relationships, $row, $key, $row_index);
+                    if($related_model instanceof $relationships[$related_key]['model']) {
+                        $model->{$relationships[$related_key]['foreign_key']} = $related_model->{$relationships[$related_key]['reference_primary_key']};
+                    } else {
+                        continue;
+                    }
+                } 
             }
-
         }
 
         return $model;
@@ -129,61 +146,82 @@ class ImportController extends Controller
         }
     }
 
-    public function parseRelationships($related_key, $relationships, $row, $key)
+    public function parseExtraColumns($relationships, $related_key, $row)
+    {
+        $result = [];
+        foreach($relationships[$related_key]['extra_columns'] as $extra_column) {
+
+            $column_matching_key = array_search($extra_column['column'], $this->column_headers);
+            $matching_data = $row[$column_matching_key];
+
+            if($column_matching_key && $matching_data) {
+
+                if(is_array($extra_column['maps_to']) && isset($extra_column['explode_on'])) {
+
+                    if (strpos(trim($matching_data), $extra_column['explode_on']) !== false) {
+
+                        $split_as_array = explode($extra_column['explode_on'], $matching_data);
+
+                        if(count($split_as_array)==2){
+                            $result = array_combine($extra_column['maps_to'], $split_as_array);
+                        }
+
+
+                        // ['first_name'=>'Kevin','last_name'=>'price']
+                    }
+
+                } else if(is_string($extra_column['maps_to'])) {
+
+                    $result[$extra_column['maps_to']] = $matching_data;
+                    // ['phone_number' => '217-369-9922']
+
+                }
+            }
+
+        }
+        return $result;
+    }
+
+    public function parseRelationships($related_key, $relationships, $row, $key, $row_index)
     {
 
-        if(is_numeric($related_key)) {
+        $related_model = new $relationships[$related_key]['model'];
+        $related_model = $related_model
+            ->where($relationships[$related_key]['reference_field'], $row[$key])
+            ->first();
+            
+            
+            /*
+             *DB::table($relationships[$related_key]['reference_table'])
+             *->where($relationships[$related_key]['reference_field'], $row[$key])
+             *->first();
+             */
 
-            $matching_related = DB::table($relationships[$related_key]['reference_table'])
-                ->where($relationships[$related_key]['reference_field'], $row[$key])
-                ->first();
+        if(!$related_model && $relationships[$related_key]['create_if_not_found']) {
 
-            if(!$matching_related && $relationships[$related_key]['create_if_not_found']) {
+            $new_related_model = new $relationships[$related_key]['model'];
+            $new_related_model->{$relationships[$related_key]['reference_field']} = $row[$key];
 
-                $new_related_model = new $relationships[$related_key]['model'];
-                $related_model_data_as_array = [];
+            if (isset($relationships[$related_key]['extra_columns']) && is_array($relationships[$related_key]['extra_columns'])) {
+                $result = $this->parseExtraColumns($relationships, $related_key, $row);
+                if(isset($result)) {
 
-                if (isset($relationships[$related_key]['extra_columns']) && is_array($relationships[$related_key]['extra_columns'])) {
+                    // validate that the column matchup exists on the related table
+                    $modelColumnsArray = $new_related_model->getConnection()->getSchemaBuilder()->getColumnListing($new_related_model->getTable());
+                    $values = array_intersect_key($result, array_flip($modelColumnsArray));
 
-                    foreach($relationships[$related_key]['extra_columns'] as $extra_column) {
-
-                        $column_matching_key = array_search($extra_column['column'], $this->column_headers);
-                        $matching_data = $row[$column_matching_key];
-
-                        if($column_matching_key && $matching_data) {
-
-                            if(is_array($extra_column['maps_to']) && isset($extra_column['explode_on'])) {
-
-                                if (strpos(trim($matching_data), $extra_column['explode_on']) !== false) {
-                                    $split_as_array = explode($extra_column['explode_on'], $matching_data);
-                                    if(count($split_as_array)==2){
-                                        $result = array_combine($extra_column['maps_to'], $split_as_array);
-                                    }
-                                    // ['first_name'=>'Kevin','last_name'=>'price']
-                                }
-
-                            } else if(is_string($extra_column['maps_to'])) {
-
-                                $result = array($extra_column['maps_to'] => $matching_data);
-                                // ['phone_number' => '217-369-9922']
-
-                            }
-                        }
-
-                        if(isset($result)) {
-
-                            // validate that the column matchup exists on the related table
-                            $modelColumnsArray = $new_related_model->getConnection()->getSchemaBuilder()->getColumnListing($new_related_model->getTable());
-                            $values = array_intersect_key($result, array_flip($modelColumnsArray));
-
-                            if($values) {
-                                foreach($values as $value_key => $value) {
-                                    $new_related_model->{$value_key} = $value;
-                                }
-                            }
-
+                    if($values) {
+                        foreach($values as $value_key => $value) {
+                            $new_related_model->{$value_key} = $value;
                         }
                     }
+                }
+            }
+
+            if (isset($relationships[$related_key]['validator']) && !empty($relationships[$related_key]['validator'])) {
+                $rules = (new $relationships[$related_key]['validator'])->rules();
+
+                if($rules) {
 
                     $messages = [];
                     if(isset($new_related_model->email)) {
@@ -193,39 +231,24 @@ class ImportController extends Controller
                         ];
 
                     }
+                    $validator = Validator::make($new_related_model->toArray(), $rules, $messages);
 
-                    if (isset($relationships[$related_key]['validator']) && !empty($relationships[$related_key]['validator'])) {
-
-                        $rules = ((new $relationships[$related_key]['validator'])->rules());
-
-                        if($rules) {
-
-                            $validator = Validator::make($new_related_model->toArray(), $rules, $messages);
-
-                            if ($validator->fails()) {
-                                $errors[] = $validator->messages();
+                    if ($validator->fails()) {
+                        return $errors[] = $validator->messages();
+                    } else {
+                        if (isset($relationships[$related_key]['append_data']) && is_array($relationships[$related_key]['append_data'])) {
+                            foreach($relationships[$related_key]['append_data'] as $column_name => $column_data) {
+                                $new_related_model->{$column_name} = $column_data;
                             }
                         }
+                        $new_related_model->save();
+                        $related_model = $new_related_model;
                     }
-
-                    if (isset($relationships[$related_key]['append_data']) && is_array($relationships[$related_key]['append_data'])) {
-                        foreach($relationships[$related_key]['append_data'] as $column_name => $column_data) {
-                            $new_related_model->{$column_name} = $column_data;
-                        }
-                    }
-                    $new_related_model->save();
-
-                    $matching_related = $new_related_model;
                 }
-
             }
-
-            if($matching_related) {
-                return $matching_related;
-            } 
-
-            return false;
         }
+
+        return $related_model;
     }
 
 }
