3f3960f0bbeab528a515ae5c7f272f1c94791ff9
cameron macfarlane <cammac1984@gmail.com>
2018-01-18 15:02:03 -0800

    removed the routes

--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-Route::group(['middleware' => ['web', 'auth', 'role:admin']], function () {
-    Route::get(config('import.get_import_route'), 'Jameron\Import\Http\Controllers\ImportController@getImport');
-    Route::post(config('import.post_import_route'), 'Jameron\Import\Http\Controllers\ImportController@postImport');
-});
