934526b6c3e4fec06ea679f7781c46eaa6e77a01
cameron macfarlane <cammac1984@gmail.com>
2018-01-22 15:35:18 -0800

    Added cleanup to the view

--- src/resources/views/upload.blade.php
+++ src/resources/views/upload.blade.php
@@ -7,7 +7,7 @@
         @slot('body')
             @if(Gate::check('upload_csv'))
                 @include('admin::partials._error_bags')
-                <form class="text-left" action="{{ url(config('import.post_import_route')) }}" method="POST" type="file"  enctype="multipart/form-data" accept=".csv">
+                <form class="text-left" action="{{ $upload_route }}" method="POST" type="file"  enctype="multipart/form-data" accept="{{ $accept }}">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     @include('import::partials.forms.import', ['submitButtonText' => 'Import', 'mode'=>'create'])
                 </form>
