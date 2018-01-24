77bd9f797f93e60aa50182be1e8df0d90a8cd0e5
cameron macfarlane <cammac1984@gmail.com>
2018-01-23 16:39:48 -0800

    added export view

---
+++ src/resources/views/partials/forms/export.blade.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+    <div class="col-md-12">
+        <label for="title">Title</label>
+        <input type="text" name="title" value="Note Title" class="form-control">
+    </div>
+</div>
+<p class="form-group">
+	<div class="col-md-12">
+		<button type="submit" class="btn btn-primary">
+			Export
+		</button>
+	</div>
+</p>
